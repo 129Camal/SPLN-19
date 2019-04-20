88dbfd4a714d80d2d3b2974d60703d811d15de26
129Camal <frederico21pinto@hotmail.com>
2019-04-20 21:54:12 +0100

    It works! It can grow, lets try. But first a push is needed! :pushpin:

--- TP2/nlp.py
+++ TP2/nlp.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3.7
 import nltk
-from nltk.corpus import stopwords
+from owlready2 import *
 
 #nltk.download('stopwords')
 #nltk.download('words')
@@ -9,12 +9,14 @@ from nltk.corpus import stopwords
 #nltk.download('averaged_perceptron_tagger')
 
 text = open("text.txt").read()
-stop = set(stopwords.words('english'))
+
 sentences = nltk.sent_tokenize(text)
 
 products = []
 prices = []
 locations = []
+j = 0 
+h = 0
 
 for sentence in sentences:
     words = nltk.word_tokenize(sentence)
@@ -48,21 +50,60 @@ for sentence in sentences:
             products.append(product)
             prices.append(price)
             flag = 0
-j = 0
+
 for product in products:
     for i in range(len(product)):
         if(i != (len(product)-1)):
             products[j] = (product[i][0], prices[j][1][0])
             j += 1
 
-j = 0
 for location in locations:
     for i in range(len(location)):
         location[i] = location[i][0]
-    locations[j] = " ".join(location)
-    j += 1
-    
+    locations[h] = "_".join(location)
+    h += 1
 
-print("Products: " + str(products))
-print("Locations: " + str(locations))
-print("Company: " + str(company))
+# OwlReady2 
+
+ontology = get_ontology("http://spln.di.uminho.pt/ontology")
+
+with ontology:
+    class Company(Thing):
+        pass
+    class Product(Thing):
+        pass
+    class Location(Thing):
+        pass
+    class has_product(ObjectProperty):
+        domain = [Company]
+        range  = [Product]
+    class has_location(ObjectProperty):
+        domain = [Company]
+        range  = [Location]
+    class has_cost(DataProperty, FunctionalProperty):
+        domain = [Product]
+        range  = [float]
+
+my_company = Company(company)
+
+for location in locations:
+    my_company.has_location.append(Location(location))
+
+for product in products:
+    produto = Product(product[0])
+    produto.has_cost = float(product[1])
+
+    my_company.has_product.append(produto)
+
+ontology.save(file = "walmart.rdf", format = "rdfxml")
+
+# print("Products: " + str(products))
+# print("Locations: " + str(locations))
+# print("Company: " + str(company) + "\n\n\n")
+# print("\nEmpresa: " + str(Company.instances()))
+# print("Produtos: " + str(Product.instances()))
+# for i in Product.instances():
+#     print("Custo do produto " + str(i) +": " + str(i.has_cost))
+# print("Locais: " + str(Location.instances()))
+# print("Localizações da Empresa: " + str(list(ontology.has_location.get_relations())))
+# print("Produtos da Empresa: " + str(list(ontology.has_product.get_relations())))
