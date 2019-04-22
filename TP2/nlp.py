287fc9f2d013d49e56d442dff07acaed286552bd
129Camal <frederico21pinto@hotmail.com>
2019-04-22 01:49:35 +0100

    Upgrades done. It can append to the ontology more than one company!

--- TP2/nlp.py
+++ TP2/nlp.py
@@ -2,27 +2,91 @@
 import nltk
 from owlready2 import *
 
+# Pacotes do nltk para fazer download
+
 #nltk.download('stopwords')
 #nltk.download('words')
 #nltk.download('maxent_ne_chunker')
-#nltk.download('punkt')
 #nltk.download('averaged_perceptron_tagger')
 
-text = open("text.txt").read()
+# Abrir ontologia
+ontology = get_ontology("http://spln.di.uminho.pt/ontology")
 
-sentences = nltk.sent_tokenize(text)
+# Especificar Classes, ObjectProperties e DataProperties
+with ontology:
+    class Company(Thing):
+        pass
+    class Product(Thing):
+        pass
+    class Location(Thing):
+        pass
+
+    class is_product_of(ObjectProperty):
+        domain = [Product]
+        range  = [Company]
+    
+    class is_location_of(ObjectProperty):
+        domain = [Location]
+        range  = [Company]
+    
+    class has_product(ObjectProperty):
+        domain = [Company]
+        range  = [Product]
+        inverse_property = is_product_of
+
+    class has_location(ObjectProperty):
+        domain = [Company]
+        range  = [Location]
+        inverse_property = is_location_of
+
+    class has_cost(DataProperty, FunctionalProperty):
+        domain = [Product]
+        range  = [float]
+
+# Abrir ficheiro com o texto
+text = open("text.txt").read()
 
 products = []
 prices = []
-locations = []
-j = 0 
-h = 0
+my_company = ""
+indexProduct = 1
 
+# Função que associa produtos e preços a uma determinada empresa na ontologia
+def addProducts():
+    global indexProduct
+    j = 0
+    
+    for product in products:
+        aux = []
+        for i in range(len(product)-1):
+            aux.append(product[i][0])
+        aux.append(str(indexProduct))
+        x = "_".join(aux)
+
+        obj = Product(x)
+        obj.has_cost = float(prices[j][1][0])
+        my_company.has_product.append(obj)
+        indexProduct += 1
+        j += 1
+    
+    del products[:]
+    del prices[:]
+
+
+# Divisão por frases
+sentences = nltk.sent_tokenize(text)
+
+#Percorrer cada frase procurando entidades ou fazendo chunks para descobrir determinadas coisas como produtos ou preços. 
 for sentence in sentences:
+
+    # Divisão por palavras
     words = nltk.word_tokenize(sentence)
 
+    # Tags de discurso
     tags = nltk.pos_tag(words)
     
+    
+    #Chunks
     #Company: {<NNP>+<NN>?}
     chunkGram = r"""Product: {<NN.*>+<IN>}
                              {<NN.*>+<V.*>}
@@ -30,13 +94,28 @@ for sentence in sentences:
     chunkParser = nltk.RegexpParser(chunkGram)
     tree = chunkParser.parse(tags)
 
+    #Entidades
     namedEnt = nltk.ne_chunk(tags)
+    
     for t in namedEnt.subtrees():
+        #Encontrar localizações
         if t.label() == "GPE":
-            locations.append(t.leaves())
-        if t.label() == "ORGANIZATION":
-            company = (t.leaves())[0][0]
+            location = t.leaves()
+            for i in range(len(location)):
+                location[i] = location[i][0]
+            local = "_".join(location)
 
+            my_company.has_location.append(Location(local))        
+                
+        #Encontrar empresas
+        if t.label() == "ORGANIZATION" or t.label() == "PERSON":
+            company = (t.leaves())[0][0]
+            if(not ontology.company):
+                if(len(products)):
+                    addProducts()
+                my_company = Company(company)
+
+    # Tratamento de dados sobre os chunks encontrados de preços e produtos            
     flag = 0
     for subtree in tree.subtrees():
         if subtree.label() == "Price": 
@@ -51,52 +130,13 @@ for sentence in sentences:
             prices.append(price)
             flag = 0
 
-for product in products:
-    for i in range(len(product)):
-        if(i != (len(product)-1)):
-            products[j] = (product[i][0], prices[j][1][0])
-            j += 1
+# Inserir na ontologia os produtos e preços da ultima empresa encontrada
+addProducts()
 
-for location in locations:
-    for i in range(len(location)):
-        location[i] = location[i][0]
-    locations[h] = "_".join(location)
-    h += 1
+# Guardar a ontologia 
+ontology.save(file = "onto.rdf", format = "rdfxml")
 
-# OwlReady2 
-
-ontology = get_ontology("http://spln.di.uminho.pt/ontology")
-
-with ontology:
-    class Company(Thing):
-        pass
-    class Product(Thing):
-        pass
-    class Location(Thing):
-        pass
-    class has_product(ObjectProperty):
-        domain = [Company]
-        range  = [Product]
-    class has_location(ObjectProperty):
-        domain = [Company]
-        range  = [Location]
-    class has_cost(DataProperty, FunctionalProperty):
-        domain = [Product]
-        range  = [float]
-
-my_company = Company(company)
-
-for location in locations:
-    my_company.has_location.append(Location(location))
-
-for product in products:
-    produto = Product(product[0])
-    produto.has_cost = float(product[1])
-
-    my_company.has_product.append(produto)
-
-ontology.save(file = "walmart.rdf", format = "rdfxml")
-
+# print("Company: " + company)
 # print("Products: " + str(products))
 # print("Locations: " + str(locations))
 # print("Company: " + str(company) + "\n\n\n")
